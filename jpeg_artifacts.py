4343c80a581f0f49c66e10d81111eb088d8a355e
Livi <livmaertens@gmail.com>
2021-10-30 15:19:25 +0200

    feat: add core camera processing and jpeg filters

---
+++ jpeg_artifacts.py
@@ -0,0 +1,37 @@
+from PIL import Image
+from io import BytesIO
+import numpy as np
+
+
+def jpeg_corruption(frame):
+    img = Image.fromarray(frame)
+    with BytesIO() as f:
+        img.save(f, format='JPEG', quality=30)
+        f.seek(0)
+        b = bytearray(f.read())
+        co = int(len(b)/10)
+        b[1000:co*2] = b[1000+co*6:co*8]
+        img = Image.open(BytesIO(b))
+    try:
+     return np.array(img)
+    except OSError:
+     return jpeg_artifacts(frame)
+
+
+def jpeg_compression(frame):
+    img = Image.fromarray(frame)
+    with BytesIO() as f:
+        img.save(f, format='JPEG', quality=1)
+        f.seek(0)
+        img = Image.open(f)
+        img.load()
+        img.save(f, format='JPEG', quality=1)
+        f.seek(0)
+        img = Image.open(f)
+        img.load()
+        img.save(f, format='JPEG', quality=1)
+        f.seek(0)
+        img = Image.open(f)
+        img.load()
+
+    return np.array(img)
