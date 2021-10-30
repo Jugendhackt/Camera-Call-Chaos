20c3e89c67d483ed9709772d8a02b71c35a1b9f2
SmarDPants <julius.kupfer@icloud.com>
2021-10-30 17:42:41 +0200

    animalfilter.py

--- org.cam-chaos/Filter/animalfilter.py
+++ org.cam-chaos/Filter/animalfilter.py
@@ -14,9 +14,12 @@ def animalfilter(frame, model):
     result_img0 = result.xyxy[0]
     for detection in result_img0:
         xmin = detection[0]
+        ymin = detection[1]
         xmax = detection[2]
+        ymax = detection[3]
 
         width = int(xmax) - int(xmin)
+        height = int(ymax)-int(ymin)
         mWidth = max(width, mWidth)
     for detection in result_img0:
         xmin = detection[0]
@@ -24,32 +27,17 @@ def animalfilter(frame, model):
         xmax = detection[2]
         ymax = detection[3]
         conf = float(detection[4])
-
-        size = (width, int(ymax)-int(ymin))
-        animal_pic_out = image.resize(size)
-
         width = int(xmax) - int(xmin)
         name = result.names[int(detection[5])]
-        if conf >= 0.5 and name == "person":
+        if conf >= 0.2 and name == "person":
             if width == mWidth:
                 label = '%s: %d%%' % (name, int(conf * 100))
                 cv2.putText(frame, label, (int(xmin), int(ymin)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
                 cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 100), 5)
             else:
+                size = (width, int(ymax) - int(ymin))
+                animal_pic_out = image.resize(size)
                 an_frame = animal_pic_out
-                frame[int(ymin):int(ymax), int(xmin):int(xmax)] = np.asanyarray(an_frame)
+                frame[int(ymin):int(ymax), int(xmin):int(xmax)] = np.array(an_frame)
     return frame
 
-vid = cv2.VideoCapture(0)
-while True:
-    ret, frame = vid.read()
-
-    frame1 = animalfilter(frame)
-
-    cv2.imshow('frame', frame1)
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-vid.release()
-cv2.destroyAllWindows()
