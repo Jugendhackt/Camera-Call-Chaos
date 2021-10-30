5e2519d1aa9d2e6fd912c1152c13807b5b48854e
SmarDPants <julius.kupfer@icloud.com>
2021-10-30 16:12:52 +0200

    Blurfilter function added, removed unnecessary lines o' code

--- org.cam-chaos/Filter/blurfilter.py
+++ org.cam-chaos/Filter/blurfilter.py
@@ -1,13 +1,8 @@
-import torch
 import cv2
+import torch
 
-# Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-
-vid = cv2.VideoCapture(0);
-while True:
-    ret, frame = vid.read();
-
+def blurfilter(frame):
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
     img = frame;
     h, w, c = img.shape
     result = model(img)
@@ -29,19 +24,14 @@ while True:
 
         width = int(xmax) - int(xmin)
         name = result.names[int(detection[5])]
-        if conf >= 0.5 and name == "person" :
+        if conf >= 0.5 and name == "person":
             if width == mWidth:
                 label = '%s: %d%%' % (name, int(conf * 100))
                 cv2.putText(frame, label, (int(xmin), int(ymin)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
                 cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 100), 5)
             else:
-                blur_frame = cv2.GaussianBlur(img[int(ymin):int(ymax), int(xmin):int(xmax)], (11, 11), 10, 10, cv2.BORDER_REFLECT)
+                blur_frame = cv2.GaussianBlur(img[int(ymin):int(ymax), int(xmin):int(xmax)], (11, 11), 10, 10,
+                                              cv2.BORDER_REFLECT)
                 frame[int(ymin):int(ymax), int(xmin):int(xmax)] = blur_frame
 
-    cv2.imshow('frame', frame)
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-vid.release()
-cv2.destroyAllWindows()
+    return frame
