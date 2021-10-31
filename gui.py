e63af45bb3583e0807c2ed1cebed5f455e64710a
BeChris100 <bechris.100@gmail.com>
2021-10-31 11:17:33 +0100

    Command Line Changes (-d: Debugger, -r: Resizeable Window)

--- gui.py
+++ gui.py
@@ -1,7 +1,10 @@
 from tkinter import *
 import threading
+import sys
 from main import run
 
+DEBUGGING = False
+RESIZEABLE = False
 
 class State:
     recording = False
@@ -13,12 +16,14 @@ app = Tk()
 windowWidth = 650
 windowHeight = 450
 
+spinnerValue = 1
 EFFECT_REFRESH_RATE = 1
 
 windowWidthSplit = windowWidth / 2
 windowHeightSplit = windowHeight / 2
 
 numberPickerSelection = IntVar()
+spinnerSelection = IntVar()
 
 WINDOW_BACKGROUND_COLOR = "#10286e"
 BUTTON_BACKGROUND_COLOR = "#30536b"
@@ -67,33 +72,55 @@ class GradientFrame(Canvas):
 
 def normalState():
     CurrentState.set("Kamerastand: Normal")
+    if DEBUGGING:
+        print("virtualCamera is back on Normal State")
 
 def glitchState():
-    CurrentState.set("Kamerastand: Glitch")
+    CurrentState.set("Kamerastand: Glitch (Epilepsy Warnung)")
+    if DEBUGGING:
+        print("glitching through the camera (epilepsy warning)")
 
 def pixelatedState():
     CurrentState.set("Kamerastand: Pixeliert")
+    if DEBUGGING:
+        print("virtualCamera has now the default pixeled state")
 
 def highPixelatedState():
     CurrentState.set("Kamerastand: Hoch-Pixeliert")
+    if DEBUGGING:
+        print("virtualCamera has now the high pixeled state")
 
 def lowPixelatedState():
     CurrentState.set("Kamerastand: Leicht Pixeliert")
+    if DEBUGGING:
+        print("virtualCamera has now the low pixeled state")
 
 def oldMovieState():
     CurrentState.set("Kamerastand: Altes Film")
+    if DEBUGGING:
+        print("setting virtualCamera filter as \"old movie\"")
 
 def colorInvertState():
     CurrentState.set("Kamerastand: Invertierte Farben")
+    if DEBUGGING:
+        print("inverting colors on virtualCamera")
 
 def objectDetectorState():
     CurrentState.set("Kamerastand: Objektmarkierer")
 
+    if DEBUGGING:
+        print("onObjectDetectorState")
+
 def blurFilterState():
     CurrentState.set("Kamerastand: Blurfilter-Effekt")
 
+    if DEBUGGING:
+        print("onBlurFilterState")
+
 def asciiState():
     CurrentState.set("Kamerastand: ASCII-Effekt")
+    if DEBUGGING:
+        print("onAsciiState")
 
 def recordStartState():
     CurrentState.set("Kamerastand: Aufzeichnen")
@@ -108,11 +135,23 @@ def goLiveState():
     CurrentState.set("Kamerastand: Live")
     state.glitch = True
 
+    if DEBUGGING:
+        print("onRecordState")
+
+def spinnerChangeListener(value):
+    spinnerValue = value
+    if DEBUGGING:
+        print("Spinner Value = " + str(spinnerValue))
+
 def refreshRateChanger():
-    # gets the number and changes
-    print(numberPickerSelection.get())
+    EFFECT_REFRESH_RATE = numberPickerSelection.get()
+    if DEBUGGING:
+        print("Effect Refresh Rate = " + str(EFFECT_REFRESH_RATE))
 
 def assign_widgets():
+    if DEBUGGING:
+        print("Assigning Widgets")
+
     currentState = Label(app, textvariable = CurrentState, background = LABEL_BACKGROUND, foreground = "#fff")
     currentState.place(x = 1, y = 1)
 
@@ -143,7 +182,7 @@ def assign_widgets():
     btn_blurFilter = Button(app, text = "Blurfilter-Effekt", border = 0, background = BUTTON_BACKGROUND_COLOR, foreground = TEXT_COLOR, command = blurFilterState)
     btn_blurFilter.place(x = 407, y = 70)
 
-    spinner_blurFilter = Scale(app, from_ = 1, to = 100, length = 142, orient = HORIZONTAL)
+    spinner_blurFilter = Scale(app, from_ = 1, to = 100, length = 142, orient = HORIZONTAL, command = spinnerChangeListener)
     spinner_blurFilter.place(x = 407, y = 98)
 
     btn_ascii = Button(app, text = "ASCII-Effekt", border = 0, background = BUTTON_BACKGROUND_COLOR, foreground = TEXT_COLOR, command = asciiState)
@@ -177,12 +216,23 @@ def create_window(title, favicon, window_geometry):
 
     assign_widgets()
 
-    app.resizable(False, False)
+    app.resizable(RESIZEABLE, RESIZEABLE)
     app.mainloop()
 
 # Window-Größe: (Horizontale Größe x Vertikale Größe)
 t = threading.Thread(target=run, args=(state,))
 t.start()
-create_window("Camera Call Chaos", "favicon.png", windowGeo)
+
+if len(sys.argv) > 1:
+    if (sys.argv[1] == "-d" or sys.argv[1] == '--debug') or (sys.argv[2] == "-d" or sys.argv[2] == '--debug'):
+        # enable debugger
+        DEBUGGING = True
+    if (sys.argv[1] == "-r" or sys.argv[1] == '--resizeable') or (sys.argv[2] == "-r" or sys.argv[2] == '--resizeable'):
+        RESIZEABLE = True
+
+if DEBUGGING:
+    print("creating window with favicon \"favicon.png\" and its window geometry on " + windowGeo)
+
+create_window("Camera.Call.Chaos", "favicon.png", windowGeo)
 
 # end file
